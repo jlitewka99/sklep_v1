da7bc031a87ce8541c12f5531b08168afe3b304f
Jakub <jlitewka99@gmail.com>
2021-01-16 04:20:20 +0100

    added additional class Category

---
+++ src/main/java/com/AdditionalComponents/Category.java
@@ -0,0 +1,19 @@
+package com.AdditionalComponents;
+
+public class Category {
+    private String category;
+    private int count;
+
+    public Category(String category, int count) {
+        this.category = category;
+        this.count = count;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getNumberOfCategories() {
+        return count;
+    }
+}
