07ae6fd99e46204b088df2f6f268afc34b24f359
Jakub <jlitewka99@gmail.com>
2021-01-19 18:48:06 +0100

    working on register

--- src/main/java/com/Sklep/jsp/User.java
+++ src/main/java/com/Sklep/jsp/User.java
@@ -3,19 +3,24 @@ package com.Sklep.jsp;
 public class User {
     private String userID;
     private String login;
+    private String password;
+    private String email;
+    private String city;
+    private String street;
+    private String postCode;
     private Status status;
-    private Double rating;
+    private Double avgRating;
+    private int numberOfRatings;
 
-    public User(String userID, String login, Status status, Double rating) { // constructor
-        this.userID = userID;
-        this.login = login;
-        this.status = status;
-        this.rating = rating;
-    }
+
 
     public User(String userID, String login) {
         this.userID = userID;
         this.login = login;
+    }
+
+    public String getStreet() {
+        return street;
     }
 
     public String getUserID() {
@@ -26,12 +31,32 @@ public class User {
         return login;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
     public Status getStatus() {
         return status;
     }
 
-    public Double getRating() {
-        return rating;
+    public Double getAvgRating() {
+        return avgRating;
+    }
+
+    public int getNumberOfRatings() {
+        return numberOfRatings;
     }
 
     public int userLoginValidate(String login, String password){    //Funkcja sprawdzająca wstępną poprawność danych logowania czyli długość loginu większa od 4 i długość hasła większa bądź równa 5.
@@ -52,7 +77,7 @@ public class User {
             }
         }
     }
-    public int userRegisterValidate(String email, String login, String password, String repeatPassword, String city, String postCode) {         //Metoda sprawdza wstępnie dane podane przez użytkownika i zwraca odpowiednie kody błędów jeśli któraś jest niepoprawna.
+    public static int userRegisterValidate(String email, String login, String password, String repeatPassword, String city, String postCode) {         //Metoda sprawdza wstępnie dane podane przez użytkownika i zwraca odpowiednie kody błędów jeśli któraś jest niepoprawna.
         if (!email.contains("@") || email.lastIndexOf('.') < email.indexOf('@')) {
             return 1;                                                       //Zwraca 1 kiedy podano niepoprawny email.
         }
@@ -89,158 +114,4 @@ public class User {
         }
         return 0;
     }
-    /*
-    public User(String password,String password2, String login, String name, String surname, String email, UserData data_br, Address address) {
-        int returner = 0;
-
-        returner = data_checking(password, password2, login, name, surname, email);//sprawdzenie poszczególnych danych czy są dobre
-        //nie wiem jak to chcecie połączyć wię cnapiszę to w taki sposób żeby łatwo było to zmienić??? - Bartek
-
-        if (returner == 0) {
-            this.password = password;
-            this.userID = "1";
-            this.status = Status.New;
-            this.login = login;
-            this.name = name;
-            this.surname = surname;
-            this.email = email;
-            this.data_br = data_br;
-            this.address = address;
-        } else {
-            System.out.println(" Mamy do czynienia z blendem nr. " + returner);
-        }
-    }
-
-
-
-
-public int data_checking(String password,String password2, String login, String name, String surname, String email)
-        {
-
-            if(password_lenght( password)){return 1;}
-            if(password_comparison(password, password2)) {return 2;}
-            if(email_monkey(email)){return 3;}
-            if(general_lenght(login)){return 4;}
-            if(general_lenght(name)){return 5;}
-            if(general_lenght(surname)){return 6;}
-            //porównania związane z adresem i data trzeba zrobić w tamtych klasach i je tu wywołać po prostu
-            return 0;
-
-        }
-
-
-public boolean password_lenght(String password)
-{
-    if(password.length() < 6) {
-        return true;
-    }
-    else
-        return false;
-}
-public boolean password_comparison(String password, String password2)
-    {
-        if(password != password2) {
-            return true;
-        }
-        else
-            return false;
-    }
-public boolean email_monkey(String email) {
-    if(email.indexOf("@") < 0) {
-        return true;
-    }
-    else
-        return false;
-}
-    public boolean general_lenght(String example)
-    {
-        if(password.length() < 3) {
-            return true;
-        }
-        else
-            return false;
-    }
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUserID() {
-        return userID;
-    }
-
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public double getOpinion() {
-        return opinion;
-    }
-
-    public void setOpinion(double opinion) {
-        this.opinion = opinion;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public UserData getData_br() {
-        return data_br;
-    }
-
-    public void setData_br(UserData data_br) {
-        this.data_br = data_br;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }*/
-    /*
-    * poprawić zgodnie z tymi danymi którę są w tej klasie teraz
-    * bo były rzeczy które rozdzielimy na inne klasy
-    * */
 }
--- src/main/java/com/grupaOdSklepu/sklep_v1/Register.java
+++ src/main/java/com/grupaOdSklepu/sklep_v1/Register.java
@@ -1,5 +1,8 @@
 package com.grupaOdSklepu.sklep_v1;
 
+import com.Sklep.jsp.Status;
+import com.Sklep.jsp.User;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -7,6 +10,17 @@ import java.io.IOException;
 
 @WebServlet(name = "register", value = "/register")
 public class Register extends HttpServlet {
+    private String userID;
+    private String login;
+    private String password;
+    private String password2;
+    private String email;
+    private String city;
+    private String postCode;
+    private String street;
+    private Double avgRating;
+    private int numberOfRatings;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -14,6 +28,15 @@ public class Register extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        login = request.getParameter("loginRegister");
+        password = request.getParameter("passwordRegister");
+        password2 = request.getParameter("password2Register");
+        email = request.getParameter("emailRegister");
+        city = request.getParameter("cityRegister");
+        street = request.getParameter("streetRegister");
+        postCode = request.getParameter("postCodeRegister");
 
+//(String email, String login, String password, String repeatPassword, String city, String postCode)
+        int statusCode = User.userRegisterValidate(email, login, password, password2, city, postCode);
     }
 }
